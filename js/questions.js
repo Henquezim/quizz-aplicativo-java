32b7d79e04d4569aabf6e309b0d2b8982d45f6a4
HENRIQUEZIM <106694756+Henquezim@users.noreply.github.com>
2023-09-26 13:25:23 -0300

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,6 @@
+//creating an array and passing the number, question, opitions, and answers
+let question ={
+  {
+  numb:1,
+  question: "Quem foi a única pessoa na história a receber a Prêmio Nobel em áreas cientificas diferrentes?",
+    answer: "Marie 
