c5d5e068e977070ed879cc190cdd9eada156d318
HENRIQUEZIM <106694756+Henquezim@users.noreply.github.com>
2023-10-03 15:31:07 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,11 @@
 //creating an array and passing the number, question, opitions, and answers
-let question ={
+let questions = [
   {
-  numb:1,
-  question: "Quem foi a única pessoa na história a receber a Prêmio Nobel em áreas cientificas diferrentes?",
-    answer: "Marie 
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
